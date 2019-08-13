ae97f458ad3c5c9536041fcef478e5cf68f1cfdf
Cristian Ospina <crisospina523@gmail.com>
2019-08-13 13:50:15 -0500

    build

---
+++ build/precache-manifest.d8c4139ca487594da7038fab1ba91b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae7f6b1f83bbb9bbdc979ff8e83e873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00478a48a123f9ddbbf0",
+    "url": "/static/css/2.ec06d838.chunk.css"
+  },
+  {
+    "revision": "712231dff7f82c057f27",
+    "url": "/static/css/main.f1f1b61e.chunk.css"
+  },
+  {
+    "revision": "00478a48a123f9ddbbf0",
+    "url": "/static/js/2.2324d130.chunk.js"
+  },
+  {
+    "revision": "712231dff7f82c057f27",
+    "url": "/static/js/main.3c9153a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a38a5e3634cf0a3353422899b6a1c9d",
+    "url": "/static/media/img1.8a38a5e3.jpg"
+  }
+]);
