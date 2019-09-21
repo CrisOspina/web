a3953d4074b8110b8c213ce0ef371e242a7e583c
Cristian Ospina <crisospina523@gmail.com>
2019-09-21 18:36:42 -0500

    [add] component-box-material-ui

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import './styles.css'
 import { ButtonSocial } from '../ButtonSocial'
+import Box from '@material-ui/core/Box'
 
 export const Footer = () => {
   return (
@@ -8,23 +9,30 @@ export const Footer = () => {
       <div className='container-fluid'>
         <div className='row'>
           <div className='col-sm-12 text-center fixed-bottom'>
-            <ButtonSocial
-              type='github'
-              size='icon'
-              href='https://github.com/CrisOspina'
-            />
+            <Box
+              boxShadow={1}
+              borderRadius='10%'
+              p={3}
+              className='hvr-underline-from-center'
+            >
+              <ButtonSocial
+                type='github'
+                size='icon'
+                href='https://github.com/CrisOspina'
+              />
 
-            <ButtonSocial
-              type='twitter'
-              size='icon'
-              href='https://twitter.com/CrisOspina523'
-            />
+              <ButtonSocial
+                type='twitter'
+                size='icon'
+                href='https://twitter.com/CrisOspina523'
+              />
 
-            <ButtonSocial
-              type='linkedin'
-              size='icon'
-              href='https://www.linkedin.com/in/cristian-ospina-38814a124/'
-            />
+              <ButtonSocial
+                type='linkedin'
+                size='icon'
+                href='https://www.linkedin.com/in/cristian-ospina-38814a124/'
+              />
+            </Box>
           </div>
         </div>
       </div>
