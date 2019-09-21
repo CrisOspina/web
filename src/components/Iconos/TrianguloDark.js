ee42ca58d5eed700558bed3e33e04aee355df9b0
Cristian Ospina <crisospina523@gmail.com>
2019-09-21 18:27:23 -0500

    [add] icons

---
+++ src/components/Iconos/TrianguloDark.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TrianguloDark () {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+    >
+      <path d='M24 22H0L12 2z' />
+    </svg>
+  )
+}
+
+export default TrianguloDark
---
+++ src/components/Iconos/TrianguloWhite.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TrianguloBlanco () {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+    >
+      <path d='M12 5.887L20.468 20H3.532L12 5.887zM12 2L0 22h24L12 2z' />
+    </svg>
+  )
+}
+
+export default TrianguloBlanco
