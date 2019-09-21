5a7d84dc58357a21bee213a8828f254ea767790f
Cristian Ospina <crisospina523@gmail.com>
2019-09-21 18:33:36 -0500

    [update]

--- src/pages/about/Me.js
+++ src/pages/about/Me.js
@@ -1,4 +1,4 @@
-export const DESCRIPCION = `The current technology is a fundamental part for each person, and I love it, that's why I like to research, try, practice and learn new technologies, especially related to the web. As a developer, I am attracted to the challenges and I try to solve them causing a good impact if it is satisfactory.  The commitment and collaborative work are fundamental to achieve the proposed objectives, both at a personal and group level.  Feel free to take a look at my projects on the web portfolio.`
+export const DESCRIPCION = `The current technology is a fundamental part for each person, and I love it, that's why I like to research, try, practice and learn new technologies, especially related to the web. As a developer, I am attracted to the challenges and I try to solve them causing a good impact.  The commitment and collaborative work are fundamental to achieve the proposed objectives, both at a personal and group level.  Feel free to take a look at my projects on the web portfolio.`
 
 export const PROFESION = `Full stack web developer.`
 
