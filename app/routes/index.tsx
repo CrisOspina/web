bd30d3e56887e5095997921648957e68639ec8e0
crisOspina <crisospina523@gmail.com>
2021-12-03 00:51:59 -0500

    feat: add pages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,39 +1,53 @@
-import type {MetaFunction, LinksFunction} from 'remix'
+import type {MetaFunction} from 'remix'
+import {Link} from 'remix'
 import {motion} from 'framer-motion'
-import styleHome from '../styles/home.css'
-
-export let links: LinksFunction = () => {
-  return [{rel: 'stylesheet', href: styleHome}]
-}
 
 export const meta: MetaFunction = () => {
   return {
-    title: 'Hi human 👋',
-    description: 'Welcome human!',
+    title: 'Hi, how are you? 👋',
+    description: 'Welcome',
+    keywords:
+      'Remix, web, portfolio, javascript, react, developer, colombia, technology, quality products',
+    'twitter:image': 'https://twitter.com/CrisOspina523/photo',
+    'twitter:title': 'Cristian Ospina',
   }
 }
 
-export default function Index() {
+const Index: React.FC = () => {
   return (
-    <section>
-      <motion.h2 className='subtitle' initial='hidden' animate='show'>
+    <section className='container'>
+      <motion.h2
+        className='text__xl text__color-secondary m-0'
+        initial='hidden'
+        animate='show'
+      >
         Hello 👋
       </motion.h2>
+
       <motion.h1
-        className='title'
+        className='text__xxl text__color-primary leading-4 m-0'
         animate={{y: 10}}
         transition={{type: 'spring', stiffness: 100}}
       >
-        I'm a <strong>software developer</strong>
+        I'm a Software Developer
       </motion.h1>
-      <motion.p className='paragraph' animate={{y: 5}}>
-        I like to solve problems through technology
+
+      <motion.p className='text__base text__color-secondary' animate={{y: 5}}>
+        and a passionate
+        <span className='text__color-primary'> Javascript developer</span> from
+        Colombia who loves to solve problems through technology and to build and
+        deliver quality products
       </motion.p>
-      <div className='container__button'>
-        <motion.button className='button' whileTap={{scale: 0.9}}>
-          ..npm run dev..
-        </motion.button>
+
+      <div className='space__center my-3'>
+        <Link to='/about'>
+          <motion.button className='button' whileTap={{scale: 0.9}}>
+            ..npm run dev..
+          </motion.button>
+        </Link>
       </div>
     </section>
   )
 }
+
+export default Index
